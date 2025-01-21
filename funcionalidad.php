7e260e9aec8778a6019e95a6b319e0258a7ad261
Iker <ikerperezcamacho.06@gmail.com>
2025-01-21 11:46:32 +0100

    Commit nueva funcionalidad

---
+++ funcionalidad.php
@@ -0,0 +1,8 @@
+<?php  
+function mostrarFecha() {  
+    // Muestra la fecha actual en la página  
+    echo "<p>La fecha y hora actual es: " . date('Y-m-d H:i:s') . "</p>";  
+}  
+
+mostrarFecha(); // Llama a la función para mostrar la fecha  
+?>
